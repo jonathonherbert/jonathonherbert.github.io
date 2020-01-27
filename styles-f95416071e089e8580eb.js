9cd03a020ddbde7726047946b1f0614d566fa96c
jonathonherbert <jonathon.herbert@guardian.co.uk>
2020-01-27 19:41:28 +0000

    Check in /public

---
+++ styles-f95416071e089e8580eb.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"0sYV":function(n,o,i){},AHrZ:function(n,o,i){},qHiR:function(n,o,i){}}]);
+//# sourceMappingURL=styles-f95416071e089e8580eb.js.map
